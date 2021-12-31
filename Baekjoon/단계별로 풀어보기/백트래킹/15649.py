2dcf90a2dddf54fa9ed2a9b4fc994ea38761843d
marunemo <chys3697@gmail.com>
2021-12-31 22:09:24 +0900

    15649번 문제 해결

--- Baekjoon/단계별로 풀어보기/백트래킹/15649.py
+++ Baekjoon/단계별로 풀어보기/백트래킹/15649.py
@@ -1,37 +1,22 @@
-from collections import Counter
-
-# 자연수 n, m 입력
-n,m = map(int, input().split())
-
-# 수열 저장
-orderedSet = [1 for _ in range(m)]
-
-# 수열 마지막 인덱스
-end = m - 1
-
-# 수열을 계속 찾을 지 확인
-isFinding = True
-
-while isFinding:
-    # 중복 확인
-    onlyOne = True
-    counter = Counter(orderedSet).most_common()
-    for i,c in counter:
-        if c != 1:
-            onlyOne = False
-            break
-    if onlyOne:
+# dfs 함수 생성
+def dfs(n, m, orderedSet, order, isVisited):
+    # depth bound 확인 및 백트래킹
+    if order == m:
         print(*orderedSet)
-    
-    # 마지막 인덱스에 1을 추가
-    orderedSet[end] += 1
-    for i in range(m):
-        # 현재 숫자가 최대를 넘어선 경우
-        if orderedSet[end - i] == n + 1:
-            # 가장 왼쪽 숫자가 최대를 넘어서면 종료
-            if i == m - 1:
-                isFinding = False
-                break
-            # 그 외에는 자신 왼쪽 숫자를 1 늘리고, 자신은 1로 초기화
-            orderedSet[end - i - 1] += 1
-            orderedSet[end - i] = 1
+        return
+    # 각 인덱스에 숫자 확인
+    for i in range(1, n + 1):
+        # 무한 루프 방지를 위한 방문 여부 확인
+        if not isVisited[i]:
+            isVisited[i] = True
+            orderedSet[order] = i
+            dfs(n, m, orderedSet, order + 1, isVisited)
+            isVisited[i] = False
+
+# 입력 값 및 초기 데이터 구조 생성
+n, m = map(int, input().split())
+orderedSet = [0 for _ in range(m)]
+isVisited = {i : False for i in range(1, n + 1)}
+
+# 함수 실행 및 결과 출력
+dfs(n, m, orderedSet, 0, isVisited)
