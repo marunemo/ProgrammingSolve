bd7ef92b788dbb29ffcd44c912566235a94b63b8
marunemo <chys3697@gmail.com>
2022-01-24 12:58:49 +0900

    2580번 문제
    - 시간 문제로 pypy3로 채점

--- Baekjoon/단계별로 풀어보기/백트래킹/2580.py
+++ Baekjoon/단계별로 풀어보기/백트래킹/2580.py
@@ -1,4 +1,5 @@
-from sys import stdin
+from sys import stdin, setrecursionlimit
+setrecursionlimit(10000)
 
 def validCheck(puzzle, x, y, currentNumber):
     for i in range(0, 9):
@@ -9,6 +10,7 @@ def validCheck(puzzle, x, y, currentNumber):
             return False
     indexX = (x // 3) * 3
     indexY = (y // 3) * 3
+
     for i in range(3):
         for j in range(3):
             if puzzle[i + indexY][j + indexX] == currentNumber:
@@ -19,27 +21,35 @@ def validCheck(puzzle, x, y, currentNumber):
 
 def sudoku(puzzle, x = 0, y = 0):
     if x == 0 and y == 9:
-        return
+        return True
     
+    isComplete = False
     if puzzle[y][x] == 0:
         for i in range(1, 10):
             if validCheck(puzzle, x, y, i):
                 puzzle[y][x] = i
                 if x == 8:
-                    sudoku(puzzle, 0, y + 1)
+                    isComplete = sudoku(puzzle, 0, y + 1)
                 else:
-                    sudoku(puzzle, x + 1, y)
+                    isComplete = sudoku(puzzle, x + 1, y)
+        if not isComplete:
+            puzzle[y][x] = 0
     else:
         if x == 8:
-            sudoku(puzzle, 0, y + 1)
+            isComplete = sudoku(puzzle, 0, y + 1)
         else:
-            sudoku(puzzle, x + 1, y)
+            isComplete = sudoku(puzzle, x + 1, y)
+
+    return isComplete
 
 puzzle = []
 for _ in range(9):
     puzzle.append(list(map(int, stdin.readline().split())))
 
-sudoku(puzzle)
+isComplete = sudoku(puzzle)
 
-for i in puzzle:
-    print(*i)
+if isComplete:
+    for i in puzzle:
+        print(*i)
+else:
+    print("Not Found!")
