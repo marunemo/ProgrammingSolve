2a0954b439c3038456ed36e53284a679d1d79daf
marunemo <chys3697@gmail.com>
2022-01-28 18:19:13 +0900

    1904번 문제 4차 시도

--- Baekjoon/단계별로 풀어보기/동적 계획법 1/1904.py
+++ Baekjoon/단계별로 풀어보기/동적 계획법 1/1904.py
@@ -7,31 +7,24 @@
 
 이때, 중복이 있는 순열은 (수 묶음의 총 개수)! / (각 수 묶음의 개수)!로 계산되며,
 00이라는 묶음이 하나 늘어날 때마다 수 묶음의 총 개수는 하나씩 줄어든다.
+
+(1 / 28 수정)
+맨 앞의 숫자를 1 또는 00으로 하면, 그 뒤의 수열은 각각 길이가 (n - 1), (n - 2)인 수열로 나타낼 수 있다.
+이를 이산수학에서의 재귀적 귀납법으로 증명한다면 다음과 같다.
+f(1) = 1, f(2) = 2 (00, 11)
+f(n) = (1) ^ f(n - 1) + (00) ^ f(n - 2)
+이때, f(n - 2)는 n이 1 또는 2 모두에 도달 가능하므로, f(n)은 존재한다.
 '''
 
-# 최대 재귀 회수 확장
-from sys import setrecursionlimit
-setrecursionlimit(3000000)
-
-# 팩토리얼 재귀 함수 생성
-def factorial(n):
-    global factorialList
-    if n not in factorialList:
-        factorialList[n] = n * factorial(n - 1)
-    return factorialList[n]
-
-# 메모이제이션
-factorialList = {0 : 1, 1 : 1}
+# 타일 가짓수 탐색
+def countTile(n):
+    a, b = 1, 2
+    for i in range(n - 1):
+        a, b = b, a + b
+    return a
 
 # 타일의 길이 입력
 n = int(input())
 
-total = 0
-for i in range(n):
-    if n < 2*i:
-        break
-    # 전체 길이가 1씩 감소하고, 해당 전체 길이에 각 중복의 개수가 1씩 증감한다.
-    total += factorial(n - i) // (factorial(i) * factorial(n - i - i))
-
 # 이진 수열의 개수를 15746으로 나눈 나머지 출력
-print(total % 15746)
+print(countTile(n) % 15746)
