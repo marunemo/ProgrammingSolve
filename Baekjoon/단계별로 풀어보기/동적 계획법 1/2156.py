e72f60970c246bc01ebb92125c66e1f560becef4
marunemo <chys3697@gmail.com>
2022-02-10 00:55:01 +0900

    2156번 문제 3차 시도

--- Baekjoon/단계별로 풀어보기/동적 계획법 1/2156.py
+++ Baekjoon/단계별로 풀어보기/동적 계획법 1/2156.py
@@ -18,6 +18,27 @@ maxGlasses[n] = max(glass[n] + maxGlasses[n - 2], glass[n] + glass[n - 1] + maxG
 이때, 마지막 잔을 꼭 마실 필요는 없으므로, 최댓값은 maxGlasses[n] 또는 maxGlasses[n - 1] 중에 있다.
 (maxGlasses[n - 2]는 maxGlasses[n]이 glass[n] + maxGlasses[n - 2]를 포함하므로, glass[n]이 음이 아닌 정수인 이상
 maxGlasses[n] >= maxGlasses[n - 2]를 항상 만족하기 때문에 고려하지 않아도 된다)
+
+=== 1차 수정 ===
+2579번 문제 때문에 헷갈린 점이 있었다.
+2579번 문제에서는 다음이나 다다음 계단으로 무조건 올라가야만 했으나, 여기서는 그렇지 않다는 점.
+그렇지만 최댓값의 범위를 처음부터 n - 2까지로 정하기에는 무리가 있기에, 범위를 정할 필요가 있었다.
+최댓값의 범위 n - 2와 n - 3은 각각 필요했고, n - 4까지도 고려할 필요가 있다.
+예로 들어
+ooxo...
+xooxo..
+ooxxo..
+와 같은 상황에서는 2칸까지는 건너뛸 필요가 있었기 때문이다.
+그러나 n - 5부터는 oxxxo와 같은 상황은 oxoxo가 다른 영향을 주지 않으면서도 항상 크거나 같은 값을 나타내므로,
+n - 4라는 조건만 고려하면 된다.
+
+이를 통해 우리가 나타낼 수 있는 경우는 다음과 같아진다.
+1. oxo...
+2. ooxo...
+3. oxxo...
+4. ooxxo...
+
+결국 연속 여부를 고려하는 방향이 가장 적절할 것이다.
 '''
 
 # fast IO
@@ -34,16 +55,24 @@ for _ in range(n):
     glass.append(wine)
 
 # 마실 수 있는 포도주의 최댓값 계산
+# 인덱스 0은 연속되지 않은 경우, 인덱스 1은 연속되는 경우를 의미
 maxGlasses = []
 for i in range(n):
     if i == 0:
-        maxGlasses.append(glass[i])
+        maxGlasses.append([glass[i], glass[i]])
     elif i == 1:
-        maxGlasses.append(glass[i] + glass[i - 1])
+        maxGlasses.append([glass[i], glass[i] + glass[i - 1]])
     elif i == 2:
-        maxGlasses.append(max(glass[i] + maxGlasses[i - 2], glass[i] + glass[i - 1]))
+        continuousGlass = glass[i] + maxGlasses[i - 1][0]
+        oneJumpGlass = glass[i] + max(maxGlasses[i - 2])
+        maxGlasses.append([oneJumpGlass, continuousGlass])
     else:
-        maxGlasses.append(max(glass[i] + maxGlasses[i - 2], glass[i] + glass[i - 1] + maxGlasses[i - 3]))
+        continuousGlass = glass[i] + maxGlasses[i - 1][0]
+        oneJumpGlass = glass[i] + max(maxGlasses[i - 2])
+        twoJumpGlass = glass[i] + max(maxGlasses[i - 3])
+        maxGlasses.append([max(oneJumpGlass, twoJumpGlass), continuousGlass])
 
 # 최대로 마실 수 있는 포도주의 양을 출력
-print(max(maxGlasses[-2:]))
+lastGlass = max(maxGlasses[-1])
+last2Glass = max(maxGlasses[-2])
+print(max(lastGlass, last2Glass))
