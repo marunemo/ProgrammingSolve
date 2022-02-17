9ea095135c2d11374c6d78096a2e8ad14771075f
marunemo <chys3697@gmail.com>
2022-02-18 00:39:46 +0900

    12865번 문제 1차 시도

--- Baekjoon/단계별로 풀어보기/동적 계획법 1/12865.py
+++ Baekjoon/단계별로 풀어보기/동적 계획법 1/12865.py
@@ -5,31 +5,41 @@ DP의 대표적인 문제인 냅색 문제이다. (배낭 문제라고도 한다
 무게가 W이고, 가치가 V라고 할 때,
 W의 합이 K 이하가 되도록 하면서 V의 합이 최대가 되도록 하는 결과를 찾으면 된다.
 
-나는 바텀업을 사용하면 쉽게 풀릴 것이라 예상한다.
-W의 범위가 (1 ≤ W ≤ 100,000)이므로, 처음을 0으로 초기화한 다음, 각각의 무게에 대한 최댓값을 구하면 될 것이라 예상한다.
+처음부터 각 무게들의 합으로 나타낼 수 있는 무게들의 최대 가치를 저장한다면 어떨까?
 '''
 
 # fast IO
 from sys import stdin
 input = stdin.readline
 
+# 물품의 수와 최대 무게를 입력
 n, k = map(int, input().split())
+
+# 각 물품들을 저장할 딕셔너리 생성
 packing = {}
 for _ in range(n):
     w, v = map(int, input().split())
-    if w not in packing or packing[w] < v:
+
+    # 처음 들어가는 무게에 대하여 중복하여 세지 않도록 설정
+    isdupl = False
+    if w not in packing:
+        packing[w] = v
+        isdupl = True
+
+    # 각 무게를 내림차순으로 정렬(계산된 무게가 뒤에서 한 번 더 계산되는 것을 방지)
+    for i in sorted(packing.keys(), reverse=True):
+        # 만약 현재 가치가 중복으로 계산될 수 있다면 계산하지 않음
+        if isdupl and i == w:
+            continue
+
+        # 만약 해당 무게를 더했을 때에도 k값 이내라면, 가치의 최대값으로 수정
+        if i + w <= k:
+            if (i + w) not in packing or packing[i + w] < packing[i] + v:
+                packing[i + w] = packing[i] + v
+    
+    # 해당 무게의 가치를 최대값으로 수정
+    if packing[w] < v:
         packing[w] = v
 
-for weight in range(1, k + 1):
-    maxWeight = 0
-    if weight in packing:
-        maxWeight = packing[weight]
-    for i in range(weight // 2 + 1):
-        if i in packing and (weight - i) in packing:
-            if maxWeight < packing[i] + packing[weight - i]:
-                maxWeight = packing[i] + packing[weight - i]
-    if maxWeight != 0:
-        packing[weight] = maxWeight
-
+# k에 가장 가까운 무게의 가치의 합을 출력
 print(packing[max(packing.keys())])
-print(packing)
