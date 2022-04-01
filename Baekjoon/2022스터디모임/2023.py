af3a1827ed0e957992f3993b3f8d7332dd7ae78b
marunemo <chys3697@gmail.com>
2022-04-01 21:17:26 +0900

    2023번 문제

---
+++ Baekjoon/2022스터디모임/2023.py
@@ -0,0 +1,24 @@
+from math import sqrt
+
+def isPrime(n):
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def awesomePrime(num, digit, n):
+    if digit == n:
+        print(num)
+
+    num *= 10
+    for i in range(10):
+        if isPrime(num + i):
+            awesomePrime(num + i, digit + 1, n)
+
+prime = [2, 3, 5, 7]
+
+n = int(input())
+
+digit = 1
+for num in prime:
+    awesomePrime(num, digit, n)
