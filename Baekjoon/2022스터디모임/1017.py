6e459727f8d53914dfd46f25c2b7f87749c5a6cd
marunemo <chys3697@gmail.com>
2022-04-02 13:15:57 +0900

    1017번 문제 4차 시도

--- Baekjoon/2022스터디모임/1017.py
+++ Baekjoon/2022스터디모임/1017.py
@@ -1,3 +1,9 @@
+'''
+시간 초과로부터 벗어나기 위한 풀이
+1. 홀수 + 홀수, 짝수 + 짝수는 모두 짝수로 2의 배수이므로, 홀수 + 짝수의 매치만 확인(최대 50개에서 최대 25개 씩으로 줄어듦)
+2. 1.의 조건이 만족하기 위해서는 홀수와 짝수의 개수가 같아야 함
+'''
+
 from math import sqrt
 from sys import stdin
 
@@ -7,48 +13,46 @@ def isPrime(n):
             return False
     return True
 
-def primePair(numCount, numList, isVisited, adjacencyList, index):
-    if index == numCount:
+def primePair(destList, srcList, maxRange, destIndex, isVisited, resultList):
+    if destIndex == maxRange:
         return True
 
-    if isVisited[index]:
-        return primePair(numCount, numList, isVisited, adjacencyList, index + 1)
-    
-    isVisited[index] = True
-    for vertex in adjacencyList[index]:
-        if not isVisited[vertex]:
-            isVisited[vertex] = True
-            if primePair(numCount, numList, isVisited, adjacencyList, index + 1):
-                isVisited[index] = False
-                isVisited[vertex] = False
-                return True
-            isVisited[vertex] = False
-    isVisited[index] = False
+    isAllMatched = False
+    for srcIndex in range(maxRange):
+        if not isVisited[srcIndex] and isPrime(destList[destIndex] + srcList[srcIndex]):
+            isVisited[srcIndex] = True
+            if primePair(destList, srcList, maxRange, destIndex + 1, isVisited, resultList):
+                isAllMatched = True
+                if destIndex == 0:
+                    resultList.append(srcList[srcIndex])
+            isVisited[srcIndex] = False
+        
+        if isAllMatched and destIndex != 0:
+            return True
     return False
-    
 
 numCount = int(stdin.readline())
 numList = list(map(int, stdin.readline().split()))
 
-isVisited = {i : False for i in range(numCount)}
+oddList, evenList = [], []
+for num in numList:
+    if num % 2 == 0:
+        evenList.append(num)
+    else:
+        oddList.append(num)
 
-adjacencyList = {i : [] for i in range(numCount)}
+isVisited = {i : False for i in range(numCount // 2)}
+maxRange = numCount // 2
+resultList = []
 
-for i in range(numCount):
-    for j in range(i + 1, numCount):
-        if isPrime(numList[i] + numList[j]):
-            adjacencyList[i].append(j)
+if maxRange == len(oddList):
+    if numList[0] % 2 == 0:
+        destList, srcList = evenList, oddList
+    else:
+        destList, srcList = oddList, evenList
+    primePair(destList, srcList, maxRange, 0, isVisited, resultList)
 
-primeSet = []
-
-isVisited[0] = True
-for vertex in adjacencyList[0]:
-    isVisited[vertex] = True
-    if primePair(numCount, numList, isVisited, adjacencyList, 1):
-        primeSet.append(numList[vertex])
-    isVisited[vertex] = False
-
-if primeSet:
-    print(*sorted(primeSet))
+if resultList:
+    print(*sorted(resultList))
 else:
     print(-1)
