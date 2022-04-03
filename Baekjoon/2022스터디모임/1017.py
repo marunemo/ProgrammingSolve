e02c697767bb885160a0574063a3de75f73d28b0
marunemo <chys3697@gmail.com>
2022-04-03 23:46:22 +0900

    1017번 문제

--- Baekjoon/2022스터디모임/1017.py
+++ Baekjoon/2022스터디모임/1017.py
@@ -2,6 +2,7 @@
 시간 초과로부터 벗어나기 위한 풀이
 1. 홀수 + 홀수, 짝수 + 짝수는 모두 짝수로 2의 배수이므로, 홀수 + 짝수의 매치만 확인(최대 50개에서 최대 25개 씩으로 줄어듦)
 2. 1.의 조건이 만족하기 위해서는 홀수와 짝수의 개수가 같아야 함
+3. adjacency matrix 생성
 '''
 
 from math import sqrt
@@ -13,21 +14,14 @@ def isPrime(n):
             return False
     return True
 
-def primePair(destList, srcList, maxRange, destIndex, isVisited, resultList):
-    if destIndex == maxRange:
-        return True
+def primePair(destEdge, srcEdge, adjList, destIndex, isVisited):
+    if isVisited[destIndex]:
+        return False
+    isVisited[destIndex] = True
 
-    isAllMatched = False
-    for srcIndex in range(maxRange):
-        if not isVisited[srcIndex] and isPrime(destList[destIndex] + srcList[srcIndex]):
-            isVisited[srcIndex] = True
-            if primePair(destList, srcList, maxRange, destIndex + 1, isVisited, resultList):
-                isAllMatched = True
-                if destIndex == 0:
-                    resultList.append(srcList[srcIndex])
-            isVisited[srcIndex] = False
-        
-        if isAllMatched and destIndex != 0:
+    for srcIndex in adjList[destIndex]:
+        if srcIndex not in srcEdge or primePair(destEdge, srcEdge, adjList, srcEdge[srcIndex], isVisited):
+            destEdge[destIndex], srcEdge[srcIndex] = srcIndex, destIndex
             return True
     return False
 
@@ -41,8 +35,8 @@ for num in numList:
     else:
         oddList.append(num)
 
-isVisited = {i : False for i in range(numCount // 2)}
 maxRange = numCount // 2
+adjacencyList = [[] for _ in range(maxRange)]
 resultList = []
 
 if maxRange == len(oddList):
@@ -50,7 +44,22 @@ if maxRange == len(oddList):
         destList, srcList = evenList, oddList
     else:
         destList, srcList = oddList, evenList
-    primePair(destList, srcList, maxRange, 0, isVisited, resultList)
+    
+    for i in range(maxRange):
+        for j in range(maxRange):
+            if isPrime(destList[i] + srcList[j]):
+                adjacencyList[i].append(j)
+
+    for pairedIndex in adjacencyList[0]:
+        destEdge, srcEdge = {0 : pairedIndex}, {pairedIndex : 0}
+        pairedCount = 1
+        for i in range(1, maxRange):
+            isVisited = {i : False for i in range(maxRange)}
+            isVisited[0] = True
+            if primePair(destEdge, srcEdge, adjacencyList, i, isVisited):
+                pairedCount += 1
+        if pairedCount == maxRange:
+            resultList.append(srcList[pairedIndex])
 
 if resultList:
     print(*sorted(resultList))
