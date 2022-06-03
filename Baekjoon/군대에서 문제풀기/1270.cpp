5ea09568ae01db5952b9e2b69572bb66a87f3bcc
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2022-06-03 19:17:55 +0900

    1270번 문제 1차 시도

---
+++ Baekjoon/군대에서 문제풀기/1270.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<int> answer;
+    
+    int ti;
+    for(int i = 0; i < n; i++) {
+        cin >> ti;
+        
+        unordered_map<int, int> milCount;
+        int mil;
+        int maxIndex = -1;
+        for(int j = 0; j < ti; j++) {
+            cin >> mil;
+            if(milCount.find(mil) == milCount.end())
+                milCount[mil] = 1;
+            else
+                milCount[mil]++;
+            
+            if(milIndex == -1 || milCount[mil] > milCount[maxIndex])
+                maxIndex = mil;
+        }
+        
+        if(milCount[maxIndex] > ti / 2)
+            answer.push_back(maxIndex);
+        else
+            answer.push_back(-1);
+    }
+    
+    for(int i = 0; i < n; i++) {
+        if(answer[i] == -1)
+            cout << "SYJKGW\n";
+        else
+            cout << answer[i] << "\n";
+    }
+    
+    return 0;
+}
