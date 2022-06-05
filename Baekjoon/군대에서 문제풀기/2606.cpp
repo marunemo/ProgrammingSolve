97dac0ddcfc93ea6f5dfcaafafa8bf6a070f2686
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2022-06-05 15:59:52 +0900

    2606번 문제 1차 시도

---
+++ Baekjoon/군대에서 문제풀기/2606.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	// FAST IO
+	cout.tie(0);
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+	
+	int comCount, netCount;
+	cin >> comCount >> netCount;
+	
+	vector<vector<bool>> linkedNetwork(comCount, vector<bool>(comCount, false));
+	
+	for(int i = 0; i < netCount; i++) {
+		int com1, com2;
+		cin >> com1 >> com2;
+		
+		// 서수를 인덱스에 맞게 수정
+		com1--;
+		com2--;
+		
+		linkedNetwork[com1][com2] = true;
+		linkedNetwork[com2][com1] = true;
+	}
+	
+	// Floyd-Warshall algorithm
+	for(int m = 0; m < comCount; m++) {
+		for(int n = 0; n < comCount; n++) {
+			if(linkedNetwork[0][m] && linkedNetwork[m][n]) {
+				linkedNetwork[0][n] = true;
+			}
+		}
+	}
+	
+	int answer = 0;
+	linkedNetwork[0][0] = false;
+	for(int i = 0; i < comCount; i++) {
+		if(linkedNetwork[0][i]) answer++;
+	}
+	
+	cout << answer << endl;
+	
+	return 0;
+}
