9182c82bdde122dcf329c69e04323ddec6ad6f64
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2022-06-06 09:25:00 +0900

    11404번 문제
    결국 우려했던 점이 맞았다.
    크게 우려했던 점은 정답으로 나올 수 있는 가중치가 2^20보다 큰 경우였다.
    2^20을 계산해보면 1,048,576으로, 100,000에 10 정도만 곱해도 쉽게 나올 수 있는 경우였다. 사실 11개의 엣지를 거칠 때, 각 가중치가 최댓값이기만 해도 쉽게 넘어버리는 숫자였다. 그리고 극단적인 경우(예로 들어, 각 가중치가 모두 최댓값(100,000)일 때, 100개를 거친다고 생각해보자.)에도 상당히 곤란해졌다. 물론, int는 2^30 정도로 설정해서 돌리면 바로 나올 수 있긴 하겠지만, 메모리로 보나 이후를 보았을 때는 0을 활용하는 방법이 가장 적절하다 생각해서 코드를 최대한 0을 활용하는 방향으로 수정했다. 생각해보면 undefined와 출발지와 목적지가 같은 경우만 0으로 처리하면 되는 것이므로, 조금은 간단했을지도 모른다.

--- Baekjoon/군대에서 문제풀기/11404.cpp
+++ Baekjoon/군대에서 문제풀기/11404.cpp
@@ -12,24 +12,26 @@ int main() {
 	int nodeCount, edgeCount;
 	cin >> nodeCount >> edgeCount;
 	
-	vector<vector<int>> adjMatrix(nodeCount, vector<int>(nodeCount, 1 << 20));
+	vector<vector<unsigned int>> adjMatrix(nodeCount, vector<unsigned int>(nodeCount, 0));
 	for(int i = 0; i < edgeCount; i++) {
-		int start, end, weight;
+		unsigned int start, end, weight;
 		cin >> start >> end >> weight;
 		
 		// 서수를 인덱스에 맞게 수정
 		start--;
 		end--;
 		
-		if(adjMatrix[start][end] > weight)
+		if(adjMatrix[start][end] == 0 || adjMatrix[start][end] > weight)
 			adjMatrix[start][end] = weight;
 	}
 	
 	for(int m = 0; m < nodeCount; m++) {
 		for(int s = 0; s < nodeCount; s++) {
 			for(int e = 0; e < nodeCount; e++) {
-				if(s != e && adjMatrix[s][e] > adjMatrix[s][m] + adjMatrix[m][e]) {
-					adjMatrix[s][e] = adjMatrix[s][m] + adjMatrix[m][e];
+				if(s != e && adjMatrix[s][m] != 0 && adjMatrix[m][e] != 0) {
+					if(adjMatrix[s][e] == 0 || adjMatrix[s][e] > adjMatrix[s][m] + adjMatrix[m][e]) {
+						adjMatrix[s][e] = adjMatrix[s][m] + adjMatrix[m][e];
+					}
 				}
 			}
 		}
@@ -37,10 +39,7 @@ int main() {
 	
 	for(int i = 0; i < nodeCount; i++) {
 		for(int j = 0; j < nodeCount; j++) {
-			if(adjMatrix[i][j] >= (1 << 20))
-				cout << 0 << " ";
-			else
-				cout << adjMatrix[i][j] << " ";
+			cout << adjMatrix[i][j] << " ";
 		}
 		cout << "\n";
 	}
