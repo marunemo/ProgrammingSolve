3a3101dbe09287e5fd36dfd48ea367bf29fbcda2
Yunseong Choe <68419358+marunemo@users.noreply.github.com>
2022-06-08 19:27:50 +0900

    1152번 문제
    문자가 없는 경우 예외 처리

--- Baekjoon/군대에서 문제풀기/1152.cpp
+++ Baekjoon/군대에서 문제풀기/1152.cpp
@@ -6,6 +6,12 @@ int main() {
 	char str[1000000];
 	cin.getline(str, 1000000, '\n');
 	
+	// 문자가 없는 경우 0을 출력
+	if(str[0] == '\0' || (str[0] == ' ' && str[1] == '\0')) {
+		cout << 0 << endl;
+		return 0;
+	}
+	
 	long long spaceCount = 0;
 	for(long long i = 0; i < 1000000 && str[i] != '\0'; i++) {
 		if(str[i] == ' ' && i != 0 && str[i + 1] != '\0')
